b14792db90ac267c009eb4f3357fed9352f3a500
CW <cwainwright26@btinternet.com>
2022-09-01 19:25:46 +0100

    Added use of Characteristic and Difficulty enums. Removed __main__ testing function.

--- src/project.py
+++ src/project.py
@@ -3,13 +3,15 @@ from __future__ import annotations
 import json
 from pathlib import Path
 import shutil
+from typing import Union
 
 import soundfile
 import numpy
 
 from metadata import Metadata
 from preferences import PREFERENCES
-from timeline import Timeline
+from timeline import Characteristic, Difficulty, Timeline
+
 
 
 class Project:
@@ -26,23 +28,56 @@ class Project:
         delim = PREFERENCES.timeline_delim
         for file in self.filepath.iterdir():
             if file.suffix == ".timeline":
+                characteristic, difficulty = file.stem.split(delim)
+                # Try loading timeline
                 try:
-                    characteristic, difficulty = file.stem.split(delim)
+                    characteristic_index = [member for member in Characteristic.__members__].index(characteristic.upper())
+                    characteristic = Characteristic(characteristic_index)
+
+                    difficulty_index = [member for member in Difficulty.__members__].index(difficulty.upper())
+                    difficulty = Difficulty(difficulty_index)                
+                    
+                    self.timelines.append(Timeline(self, characteristic, difficulty))
+                # If timeline not recognised throw error
                 except ValueError:
-                    characteristic = "Normal"
-                    difficulty = file.stem
-                self.timelines.append(Timeline(self, characteristic, difficulty))
+                    ValueError(f"{file.stem} characteristic or difficulty not recognised!")
 
         if len(self.timelines) == 0:
-            self.timelines.append(Timeline(self, "Standard", "Easy"))
-
-        self.timelines[-1][0].new_rest(
-            "Intro Beats", PREFERENCES.start_rest_duration
-        )
+            self.timelines.append(Timeline(self, Characteristic.STANDARD, Difficulty.EASY))
+            
+        self.characteristic = self.timelines[-1].characteristic
+        self.difficulty = self.timelines[-1].difficulty
 
     @property
     def namelist(self) -> list[str]:
-        return self.filepath.iterdir()
+        return [file.name for file in self.filepath.iterdir()]
+    
+    @property
+    def current_timeline(self) -> Timeline:
+        index = [timeline.id() for timeline in self.timelines].index((self.characteristic, self.difficulty))
+        return self.timelines[index]
+ 
+    def set_timeline(self, characteristic: Union(int, Characteristic), difficulty: Union(int, Difficulty)) -> Timeline:
+        if self.current_timeline.empty():
+            self.timelines.remove(self.current_timeline)
+            
+        # Reset Characteristic and Difficulty
+        if isinstance(characteristic, int):
+            characteristic = Characteristic(characteristic)
+        self.characteristic = characteristic
+        if isinstance(difficulty, int):
+            difficulty = Difficulty(difficulty)
+        self.difficulty = difficulty
+
+        # If timeline exists:
+        if (characteristic, difficulty) in [timeline.id() for timeline in self.timelines]:
+            index = [timeline.id() for timeline in self.timelines].index((characteristic, difficulty))
+            return self.timelines[index]
+
+        # If timeline does not exist:
+        else:
+            self.timelines.append(Timeline(self, characteristic, difficulty))
+            return self.timelines[-1]
 
     def write_json(self, filename: str, data: object, indent: int = None) -> bool:
         with open(self.filepath / filename, "w") as file:
@@ -76,23 +111,3 @@ class Project:
 
     def delete(self) -> None:
         shutil.rmtree(self.filepath)
-
-
-def main():
-    from section import Section
-    from robject import load_robject
-
-    project = Project(name="test", tempo=128, start_beat=4)
-    project.metadata["_songName"] = "hello_world"
-    project.metadata["_songFilename"] = "hello_world.wav"
-    project.metadata.save()
-    index = project.timelines[0].add_section(Section("Chorus"))
-    project.timelines[0][index].add_robject(
-        load_robject("Gallop", "[0.25, 0.25, 0.5]", True)
-    )
-    project.timelines[0][index].add_robject(load_robject("1 beat", "[]"))
-    project.timelines[0].save()
-    input("Press enter to delete test project...")
-
-if __name__ == "__main__":
-    main()
