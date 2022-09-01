1bbb9434f4db8f202ede6f1698f45f5af3613214
CW <cwainwright26@btinternet.com>
2022-09-01 19:48:19 +0100

    Removed unused import of json. Added support for Characteristic and Difficulty enums. Added get_data and populate_tree functions. Removed __main__ testing function.

--- src/timeline.py
+++ src/timeline.py
@@ -1,5 +1,8 @@
-import json
 from copy import deepcopy
+from enum import Enum
+from typing import List, Tuple
+
+from PyQt6.QtWidgets import QTreeWidget, QTreeWidgetItem
 
 import files
 from preferences import PREFERENCES
@@ -9,8 +12,26 @@ from section import Section
 TIMELINETEMPLATE = files.get_template("timeline")
 
 
+class Characteristic(Enum):
+    STANDARD = 0
+    ONESABER = 1
+    DEGREES180 = 2
+    DEGREES360 = 3
+
+
+class Difficulty(Enum):
+    EASY = 0
+    NORMAL = 1
+    HARD = 2
+    EXPERT = 3
+    EXPERTPLUS = 4
+
+    def rank(self):
+        return (2 * self.value) - 1
+
+
 class Timeline:
-    def __init__(self, project, characteristic: str, difficulty: str):
+    def __init__(self, project, characteristic: Characteristic, difficulty: Difficulty):
         self.__project = project
         self.characteristic = characteristic
         self.difficulty = difficulty
@@ -18,21 +39,38 @@ class Timeline:
         self.selected_section: int = None
         self.selected_robject: int = None
         self.robject_backup: dict = []
-        if self.filename not in self.__project.namelist:
-            with open(self.__project.filepath / self.filename, "w") as file:
-                json.dump(TIMELINETEMPLATE, file, indent=4)
-        self.load()
+        if self.filename in self.__project.namelist:
+            self.load()
+        else:
+            intro = Section("Intro").new_rest(
+                "Intro Beats", PREFERENCES.start_rest_duration
+            )
+            self.add_section(intro)
 
     @property
     def filename(self) -> str:
-        return f"{self.characteristic}{PREFERENCES.timeline_delim}{self.difficulty}.timeline"
+        return self.__str__() + ".timeline"
 
-    def __getitem__(self, index: int) -> Section:
-        return self.sections[index]
+    def __len__(self) -> int:
+        return len(self.sections)
+
+    def id(self) -> Tuple[Characteristic, Difficulty]:
+        return (self.characteristic, self.difficulty)
+
+    def __str__(self) -> str:
+        return (
+            self.characteristic.name + PREFERENCES.timeline_delim + self.difficulty.name
+        )
+
+    def empty(self) -> bool:
+        return len(self) == 1 and len(self.sections[0]) == 1
 
     def load(self):
         """Load Timeline from Project"""
         timeline_file = self.__project.read_json(self.filename)
+
+        # Load robject backups
+        self.robject_backup = timeline_file.get("robject_backup", [])
 
         # Load main timeline
         for section_data in timeline_file.get("sections", []):
@@ -46,9 +84,6 @@ class Timeline:
                     robject = restore_robject(self.robject_backup, id, category, mirror)
                 section.add_robject(robject)
             self.add_section(section)
-
-        # Load robject backups
-        self.robject_backup = timeline_file.get("robject_backup", [])
 
     def save(self):
         """Save Timeline to Project"""
@@ -73,7 +108,7 @@ class Timeline:
     def add_section(self, section: Section) -> int:
         """Add Section to Timeline"""
         self.sections.append(section)
-        return len(self.sections)-1
+        return section
 
     def new_section(self, name: str) -> int:
         """Add Section to Timeline (without accessing Section type)"""
@@ -86,11 +121,27 @@ class Timeline:
             return True
         return False
 
-    def export_timeline(self) -> dict:
-        self.__project.sub_dir("beatmap_data")
+    def get_data(self, time: float) -> List[Tuple[float, str, List]]:
+        start_time = time
+        contents = []
+        for section in self.sections:
+            contents.append((start_time, section.name, section.apply_time(start_time)))
+            start_time += section.duration
+        return contents
 
-
-if __name__ == "__main__":
-    from project import main
-
-    main()
+    def populate_tree(self, tree_widget: QTreeWidget):
+        data = self.get_data(0)
+        for section in data:
+            tree_section = QTreeWidgetItem([str(section[0]), section[1]])
+            for robject in section[2]:
+                tree_robject = QTreeWidgetItem(
+                    [
+                        str(robject[0]),
+                        robject[1].robject_id,
+                        robject[1].robject_category,
+                        robject[1].type.name.lower(),
+                        str(robject[1].duration),
+                    ]
+                )
+                tree_section.addChild(tree_robject)
+            tree_widget.addTopLevelItem(tree_section)
